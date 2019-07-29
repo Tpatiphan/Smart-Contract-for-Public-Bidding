de3ff51321845467f1bb677e41abb170e008571f
utkuglsvn <utku.glsvn@gmail.com>
2019-07-29 16:56:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 //console.log(web3)
+
+
+
+function connectedControl()
+{
+    if(!web3.connected())
+    {
+        alert("Bağlantı sağlanamadı")
+        console.log("not connected")
+    }
+}
+
 web3.eth.getAccounts(console.log);//public key
 
+
 //console.log(web3);
-function Warn() {
+function BlockControl() {
     if(web3.eth.isSyncing().then(console.log)==false)
     {
         alert("block yok");
     }    
 }
 web3.eth.defaultAccount = '0x9Eb5410e156E27Ca5F608611bbfF4D27b7543879';
-web3 . eth . getBalance ( '0x9Eb5410e156E27Ca5F608611bbfF4D27b7543879' )
-. then ( console . log );// para çekme
+/*web3 . eth . getBalance ( '0x9Eb5410e156E27Ca5F608611bbfF4D27b7543879' )
+. then ( console . log );// para çekme*/
 
+function getMoney(address){
+    web3 . eth . getBalance ( address )
+    . then ( console . log );// para çekme
+}
 
-web3 . eth . sendTransaction ({ //send traction
-    from : '0x9Eb5410e156E27Ca5F608611bbfF4D27b7543879' ,
-    to : '0xb02089163F7Be745742897fC85E95108AbAA4d83' ,
-    value : '1000000000000000'
-});
+function sendTransaction(fromadress,toaddress) {
+    web3 . eth . sendTransaction ({ //send traction
+        from : fromadress,
+        to : toaddress ,
+        value : '1000000000000000'
+    });
+  }
+  
+  let toDo;
+
+  window.addEventListener("load", () => {
+    toDo = new ToDoClass();
+  });
